81216c181b40184b0ec129d97657a25ff789dee2
kalilBelgoumri <kalil.belgoumri@gmail.com>
2021-10-29 09:07:02 +0200

    i have setState in burgerMenu

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { Switch, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import React from 'react';
-import imgHamburger from './assets/hambuger.png';
-
+// import imgHamburger from './assets/hambuger.png';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -14,22 +14,23 @@ function App() {
       <header>
         <div className="bg-secondary h-24 flex items-center justify-around">
           <Navbar />
-          <div className="md:hidden hamburger__menu flex justify-end items-center">
+          {/* <div className="md:hidden hamburger__menu flex justify-end items-center">
             <img
               className="cursor-pointer"
               src={imgHamburger}
               alt="Hamburger"
             />
-          </div>
+          </div> */}
         </div>
       </header>
       <main className="bg-primary h-24 min-h-screen w-full">
-      <div className="nom flex justify-start text-white my-20 mx-52 text-3xl font-bold">
-        <h1>I AM BELGOUMRI KALIL </h1>
-      </div>
-      <div className="nom flex justify-start text-white my-20 mx-52 text-3xl font-bold">
-        <h1>WEB DEVELOPPER FULL STACK </h1>
-      </div>
+        <div className="nom flex justify-start text-white my-20 mx-52 text-3xl font-bold">
+          <h1>I AM BELGOUMRI KALIL </h1>
+        </div>
+        <div className="nom flex justify-start text-white my-20 mx-52 text-3xl font-bold">
+          <h1>WEB DEVELOPPER FULL STACK </h1>
+        </div>
+        <Button inverted>Standard</Button>
       </main>
       <footer className="bg-secondary h-24 min-h-96 w-full"></footer>
     </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,34 @@
 import { NavLink } from 'react-router-dom';
 import React from 'react';
 import img from '../assets/Logo.png';
+import { useState, useEffect } from 'react';
+import imgHamburger from '../assets/hamburger.png'
+
 
 const Navbar = () => {
+
+  const [toggleMenu, setToggleMenu] = useState(false);
+  const toggleSmallScreen = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
     <>
       <div>
+      <div className="md:hidden hamburger__menu flex justify-end items-center">
+              <img
+                className="cursor-pointer"
+                onClick={toggleSmallScreen}
+                src={imgHamburger}
+                alt="Hamburger"
+              />
+            </div>
         <div className="invisible md:visible logo__Nav absolute left-0 -mt-3 mx-6">
           <img className="h-1/2" src={img} alt="Logo" />
         </div>
         <nav>
-          <ul className=" text-white flex justify-evenly items-center hidden md:flex">
+          {toggleMenu && ( 
+            <ul className=" text-white flex justify-evenly items-center hidden md:flex">
             <li>
               <NavLink exact to="/">
                 Acceuil
@@ -32,7 +50,9 @@ const Navbar = () => {
               </NavLink>
             </li>
           </ul>
+        )}
         </nav>
+         
       </div>
     </>
   );
