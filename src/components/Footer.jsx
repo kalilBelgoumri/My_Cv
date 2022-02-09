52cf54a2bd57ad0bd099094cc3f623f9c0149eed
kalilBelgoumri <kalil.belgoumri@gmail.com>
2022-02-09 21:24:18 +0100

    update Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import ContactUs from '../components/ContactUs'
 function Footer() {
   return (
     <div>
-      <footer className="bg-gradient-to-b from-gray-700 via-gray-900 to-black flex justify-center items-center py-20">
+      <footer className="bg-gradient-to-b from-gray-700 via-gray-900 to-black flex justify-center items-center py-20 px-10">
         <ContactUs />
       </footer>
     </div>
